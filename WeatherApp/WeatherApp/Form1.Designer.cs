c04d4e735a9b22106d05646949a877ad80cfc502
riddar13 <40752707+riddar13@users.noreply.github.com>
2019-11-26 21:29:39 +0600

    add geolocation

--- WeatherApp/WeatherApp/Form1.Designer.cs
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -41,9 +44,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -59,6 +60,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.textBox1);
@@ -77,6 +79,34 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Погода";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(732, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 81);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Моя локация";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(644, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 26);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(462, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 26);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label5
             // 
@@ -180,31 +210,15 @@
             this.panel1.Size = new System.Drawing.Size(90, 90);
             this.panel1.TabIndex = 0;
             // 
-            // textBox1
+            // label8
             // 
-            this.textBox1.Location = new System.Drawing.Point(462, 156);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(166, 26);
-            this.textBox1.TabIndex = 10;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(644, 156);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(88, 26);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Поиск";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(732, 217);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(154, 81);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "Моя локация";
-            this.button4.UseVisualStyleBackColor = true;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label8.Location = new System.Drawing.Point(454, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 46);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "label8";
             // 
             // Form1
             // 
@@ -240,6 +254,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- WeatherApp/WeatherApp/Form1.cs
+++ WeatherApp/WeatherApp/Form1.cs
@@ -21,6 +21,8 @@ namespace WeatherApp
         public double lat;
         public double tempC;
         public double tempF;
+        public String city;
+        public String an;
         public bool trig = true;
 
         public Form1()
@@ -36,70 +38,87 @@ namespace WeatherApp
             
         }
 
+        public async void handler(String queue)
+        {
+            WebRequest request = WebRequest.Create(queue);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-urlencoded";
+            try
+            {
+                WebResponse response = await request.GetResponseAsync();
+                string answer = string.Empty;
+                using (Stream s = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(s))
+                    {
+                        answer = await reader.ReadToEndAsync();
+                    }
+                }
+                response.Close();
+
+                an = answer;
+
+                OpenWeather.OpenWeather oW = JsonConvert.DeserializeObject<OpenWeather.OpenWeather>(answer);
+                city = oW.name;
+                label8.Text = this.city;
+                tempF = (oW.main.temp - 32)*5/9;
+                tempC = oW.main.temp;
+
+                panel1.BackgroundImage = oW.weather[0].Icon;
+                label1.Text = oW.weather[0].main;
+                label2.Text = oW.weather[0].description;
+
+                if (trig)
+                    label3.Text = "Средняя температура: " + oW.main.temp.ToString("0.##") + "°С";
+                else
+                    label3.Text = "Средняя температура: " + tempF.ToString("0.##") + "°F";
+
+                label6.Text = "Скорость: " + oW.wind.speed.ToString() + " м/с";
+                label7.Text = "Направление: " + oW.wind.deg.ToString() + "°";
+                label4.Text = "Влажность: " + oW.main.humidity.ToString() + "%";
+                label5.Text = "Давление: " + ((int)oW.main.pressure).ToString() + " мм рт.ст.";
+            }
+            catch (System.Net.WebException e)
+            {
+                MessageBox.Show(e.Message + "Город не найден");
+            }
+        }
+
         private async void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-            WebRequest request = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=Moscow&APPID=1499f87b39982a746c16f0c3ff09b18b");
-
-            request.Method = "POST";
-
-            request.ContentType = "application/x-www-urlencoded";
-
-            WebResponse response = await request.GetResponseAsync();
-
-            string answer = string.Empty;
-
-            using (Stream s = response.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader(s))
-                {
-                    answer = await reader.ReadToEndAsync();
-                }
-            }
-
-            response.Close();
-
-            richTextBox1.Text = answer;
-
-            OpenWeather.OpenWeather oW = JsonConvert.DeserializeObject<OpenWeather.OpenWeather>(answer);
-
-            panel1.BackgroundImage = oW.weather[0].Icon;
-
-            label1.Text = oW.weather[0].main;
-
-            label2.Text = oW.weather[0].description;
-
-            tempF = oW.main.temp + 273.15;
-
-            if (trig)
-                label3.Text = "Средняя температура: " + oW.main.temp.ToString("0.##") + "°С";
-            else
-                label3.Text = "Средняя температура: " + tempF.ToString("0.##") + "°F";
-
-           // tempC = oW.main.temp;
-           // tempF = oW.main.temp + 273.15;
-
-            label6.Text = "Скорость: " + oW.wind.speed.ToString() + " м/с";
-            label7.Text = "Направление: " + oW.wind.deg.ToString() + "°";
-            label4.Text = "Влажность: " + oW.main.humidity.ToString() + "%";
-            label5.Text = "Давление: " + ((int)oW.main.pressure ).ToString() + " мм рт.ст.";
-
+            richTextBox1.Text = an;    
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //label3.Text = "Средняя температура: " + tempC.ToString("0.##") + "°С";
             trig = true;
+            label3.Text = "Средняя температура: " + tempC.ToString("0.##") + "°С";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //label3.Text = "Средняя температура: " + tempF.ToString("0.##") + "°F";
             trig = false;
+            label3.Text = "Средняя температура: " + tempF.ToString("0.##") + "°F";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            String newCity;
+            newCity = this.city;
+            textBox1.Clear();
+            String queue = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&APPID=1499f87b39982a746c16f0c3ff09b18b&lang=ru";
+            handler(queue);
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String queue = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=1499f87b39982a746c16f0c3ff09b18b";
+            handler(queue);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            city = textBox1.Text;
         }
     }
 }
