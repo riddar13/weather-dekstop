9236a632443ccfd64b716a73805d61f32c6deb93
riddar13 <40752707+riddar13@users.noreply.github.com>
2019-11-26 15:57:27 +0600

    update class OpenWeather

--- WeatherApp/WeatherApp/OpenWeather/OpenWeather.cs
+++ WeatherApp/WeatherApp/OpenWeather/OpenWeather.cs
@@ -9,13 +9,16 @@ namespace WeatherApp.OpenWeather
 {
     class OpenWeather
     {
+        public coord coord;
+        public weather[] weather;
         [JsonProperty("base")]
         public string Base;
-
+        public main main;
         public double visibility;
-
+        public wind wind;
+        public clouds clouds;
         public double dt;
-
+        public sys sys;
         public int id;
 
         public string name;
